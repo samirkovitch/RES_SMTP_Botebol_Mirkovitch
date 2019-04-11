3112e92461729d4f002f290bdad2ebed17a7476a
BasileBotebol <basile.botebol@gmail.com>
2019-04-11 10:48:02 +0200

    adding almost finished configmanager

--- RES_Lab_smtp/src/config/ConfigManager.java
+++ RES_Lab_smtp/src/config/ConfigManager.java
@@ -1,8 +1,103 @@
 package config;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import model.mail.Person;
+
 public class ConfigManager {
+    private String smtpServerAddress;
+    private int port;
+    private List<String> message;
+    private List<Person> victims;
+    private List<Person> witness;
+    private int nbGroups;
+    private String victimRESFile;
+    private String configFile;
+    private String messageFile;
+    private String victimFile;
 
 
+
+public ConfigManager(String victimRESFile, String configFile, String messageFile, String victimFile) throws IOException {
+    this.victimRESFile = victimRESFile;
+    this.configFile = configFile;
+    this.messageFile = messageFile;
+    this.victimFile = victimFile;
+    victims = loadAddresses(victimFile);
+    message = loadMessage (messageFile);
+    loadProperties(configFile);
+}
+
+    private List<String> loadMessage(String file) throws IOException {
+        List<String> result = new ArrayList<String>();
+        FileInputStream f = new FileInputStream(file);
+        InputStreamReader i = new InputStreamReader(f, "UTF-8");
+        BufferedReader b = new BufferedReader(i);
+        String line = b.readLine();
+        while (line != null){
+            String individualMessage = "";
+            while (line != null && !line.equals("---")){
+             individualMessage += line + "\r\n";
+             line = b.readLine();
+            }
+            result.add(individualMessage);
+            line = b.readLine();
+        }
+        return result; 
+    }
+
+    private List<Person> loadAddresses(String file) throws IOException {
+    List<Person> result = new ArrayList<Person>();
+    FileInputStream f = new FileInputStream(file);
+    InputStreamReader i = new InputStreamReader(f, "UTF-8");
+    BufferedReader b = new BufferedReader(i);
+    String address = b.readLine();
+    while (address != null){
+        result.add(new Person(address));
+        address = b.readLine();
+    }
+    return result;
+    }
+
+    private void loadProperties(String file) throws IOException {
+        FileInputStream f = new FileInputStream(file);
+        Properties properties = new Properties();
+        properties.load(f);
+        this.smtpServerAddress = properties.getProperty("smtpServerAddress");
+        this.port = Integer.parseInt(properties.getProperty("smtpServerPort"));
+        this.nbGroups = Integer.parseInt(properties.getProperty("numberOfGroups"));
+        this.witness = new ArrayList();
+        String[] witnessAdresses = properties.getProperty("witnessToCC").split(",");
+        for(String address : witnessAdresses){
+            this.witness.add(new Person(address));
+        }
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
